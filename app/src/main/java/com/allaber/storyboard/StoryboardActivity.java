bb9ab02e81a9bffbc264f43ff362a2b9e0f8e1a8
ALLABER <ubermenschalone@gmail.com>
2022-09-03 01:50:39 +0500

    Добавил тёмную тему

--- app/src/main/java/com/allaber/storyboard/StoryboardActivity.java
+++ app/src/main/java/com/allaber/storyboard/StoryboardActivity.java
@@ -5,6 +5,8 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -27,7 +29,25 @@ public class StoryboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        elementInitialization();
 
+    }
+
+    private void elementInitialization(){
+        if(hasInternetConnection()) {
+            Toast.makeText(this, getString(R.string.string_no_internet_connection), Toast.LENGTH_LONG).show();
+        } else {
+            loadData();
+        }
+    }
+
+    protected boolean hasInternetConnection() {
+        String connectivityService = Context.CONNECTIVITY_SERVICE;
+        ConnectivityManager systemService = (ConnectivityManager) getSystemService(connectivityService);
+        return systemService.getActiveNetworkInfo() == null;
+    }
+
+    private void loadData() {
         Services services = new Services();
         StoriesApi storiesApi = services.getStoriesApi();
 
@@ -38,21 +58,20 @@ public class StoryboardActivity extends AppCompatActivity {
             public void onResponse(Call<Root> call, Response<Root> response) {
                 if(response.isSuccessful()) {
                     List<Story> stories = response.body().getDetail().getStories();
-                    RecyclerView rvContacts = (RecyclerView) findViewById(R.id.recyclerView);
+                    RecyclerView rvContacts = findViewById(R.id.recyclerView);
                     StoriesAdapter adapter = new StoriesAdapter(stories);
                     rvContacts.setAdapter(adapter);
                     RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getApplicationContext(), 3);
                     rvContacts.setLayoutManager(mLayoutManager);
                 } else {
-                    Toast.makeText(getApplicationContext(), "Error: " + response.message(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), getString(R.string.string_error) + response.message(), Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onFailure(Call<Root> call, Throwable t) {
-                Toast.makeText(getApplicationContext(), "Unknown error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), getString(R.string.string_error) + t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
-
     }
 }
