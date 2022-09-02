032beefc76d4d1e22982e31de13b500bf106f508
ALLABER <ubermenschalone@gmail.com>
2022-09-02 15:52:17 +0500

    Initial commit

---
+++ app/src/main/java/com/allaber/storyboard/StoryboardActivity.java
@@ -0,0 +1,14 @@
+package com.allaber.storyboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StoryboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
